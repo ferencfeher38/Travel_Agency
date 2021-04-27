4408334a74255c5e1182bdfc10c15bb331dcc336
david <lakodevid@gmail.com>
2021-04-27 17:22:41 +0200

    Hotelek kilistazasa.

--- hotels.php
+++ hotels.php
@@ -1,4 +1,10 @@
-<?php include_once "includes/header.php"?>
+<?php include_once "includes/header.php";
+
+include_once "database/db.php";
+$db = db::get();
+$queryString = "SELECT * FROM HOTEL";
+$result = $db->query($queryString);
+?>
 			
 			  
 			<!-- start banner Area -->
@@ -23,324 +29,68 @@
 		            <div class="row d-flex justify-content-center">
 		                <div class="menu-content pb-40 col-lg-8">
 		                    <div class="title text-center">
-		                        <h1 class="mb-10">Popular Destinations</h1>
-		                        <p>We all live in an age that belongs to the young at heart. Life that is becoming extremely fast, day to.</p>
+		                        <h1 class="mb-10">Népszerű hotelek</h1>
 		                    </div>
 		                </div>
 		            </div>						
 					<div class="row">
-						<div class="col-lg-4">
-							<div class="single-destinations">
-								<div class="thumb">
-									<img src="img/hotels/d1.jpg" alt="">
-								</div>
-								<div class="details">
-									<h4 class="d-flex justify-content-between">
-										<span>Hilton Star Hotel</span>                              	
-										<div class="star">
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star"></span>				
-										</div>	
-									</h4>
-									<p>
-										View on map   |   49 Reviews
-									</p>
-									<ul class="package-list">
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Swimming pool</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Gymnesium</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Wi-fi</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Room Service</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Air Condition</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Restaurant</span>
-											<span>Yes</span>
-										</li>												
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Price per night</span>
-											<a href="#" class="price-btn">$250</a>
-										</li>													
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4">
-							<div class="single-destinations">
-								<div class="thumb">
-									<img src="img/hotels/d2.jpg" alt="">
-								</div>
-								<div class="details">
-									<h4 class="d-flex justify-content-between">
-										<span>Hilton Star Hotel</span>                              	
-										<div class="star">
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star"></span>				
-										</div>	
-									</h4>
-									<p>
-										View on map   |   49 Reviews
-									</p>
-									<ul class="package-list">
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Swimming pool</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Gymnesium</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Wi-fi</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Room Service</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Air Condition</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Restaurant</span>
-											<span>Yes</span>
-										</li>												
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Price per night</span>
-											<a href="#" class="price-btn">$250</a>
-										</li>													
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4">
-							<div class="single-destinations">
-								<div class="thumb">
-									<img src="img/hotels/d3.jpg" alt="">
-								</div>
-								<div class="details">
-									<h4 class="d-flex justify-content-between">
-										<span>Hilton Star Hotel</span>                              	
-										<div class="star">
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star"></span>				
-										</div>	
-									</h4>
-									<p>
-										View on map   |   49 Reviews
-									</p>
-									<ul class="package-list">
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Swimming pool</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Gymnesium</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Wi-fi</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Room Service</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Air Condition</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Restaurant</span>
-											<span>Yes</span>
-										</li>												
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Price per night</span>
-											<a href="#" class="price-btn">$250</a>
-										</li>													
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4">
-							<div class="single-destinations">
-								<div class="thumb">
-									<img src="img/hotels/d4.jpg" alt="">
-								</div>
-								<div class="details">
-									<h4 class="d-flex justify-content-between">
-										<span>Hilton Star Hotel</span>                              	
-										<div class="star">
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star"></span>				
-										</div>	
-									</h4>
-									<p>
-										View on map   |   49 Reviews
-									</p>
-									<ul class="package-list">
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Swimming pool</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Gymnesium</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Wi-fi</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Room Service</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Air Condition</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Restaurant</span>
-											<span>Yes</span>
-										</li>												
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Price per night</span>
-											<a href="#" class="price-btn">$250</a>
-										</li>													
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4">
-							<div class="single-destinations">
-								<div class="thumb">
-									<img src="img/hotels/d5.jpg" alt="">
-								</div>
-								<div class="details">
-									<h4 class="d-flex justify-content-between">
-										<span>Hilton Star Hotel</span>                              	
-										<div class="star">
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star"></span>				
-										</div>	
-									</h4>
-									<p>
-										View on map   |   49 Reviews
-									</p>
-									<ul class="package-list">
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Swimming pool</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Gymnesium</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Wi-fi</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Room Service</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Air Condition</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Restaurant</span>
-											<span>Yes</span>
-										</li>												
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Price per night</span>
-											<a href="#" class="price-btn">$250</a>
-										</li>													
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4">
-							<div class="single-destinations">
-								<div class="thumb">
-									<img src="img/hotels/d6.jpg" alt="">
-								</div>
-								<div class="details">
-									<h4 class="d-flex justify-content-between">
-										<span>Hilton Star Hotel</span>                              	
-										<div class="star">
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star checked"></span>
-											<span class="fa fa-star"></span>				
-										</div>	
-									</h4>
-									<p>
-										View on map   |   49 Reviews
-									</p>
-									<ul class="package-list">
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Swimming pool</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Gymnesium</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Wi-fi</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Room Service</span>
-											<span>No</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Air Condition</span>
-											<span>Yes</span>
-										</li>
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Restaurant</span>
-											<span>Yes</span>
-										</li>												
-										<li class="d-flex justify-content-between align-items-center">
-											<span>Price per night</span>
-											<a href="#" class="price-btn">$250</a>
-										</li>													
-									</ul>
-								</div>
-							</div>
-						</div>																														
+
+                        <?php foreach($result as $row): ?>
+                        <div class="col-lg-4">
+                            <div class="single-destinations">
+                                <div class="thumb">
+                                    <img src="img/hotels/d5.jpg" alt="">
+                                </div>
+                                <div class="details">
+                                    <h4 class="d-flex justify-content-between">
+                                        <span><?php echo $row["HOTEL_NAME"] ?></span>
+                                        <!--<div class="star">
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star"></span>
+                                        </div>-->
+                                    </h4>
+                                    <p>
+                                        View on map   |   49 Reviews
+                                    </p>
+                                    <ul class="package-list">
+                                        <li class="d-flex justify-content-between align-items-center">
+                                            <span>Medence</span>
+                                            <span>Igen</span>
+                                        </li>
+                                        <li class="d-flex justify-content-between align-items-center">
+                                            <span>Konditerem</span>
+                                            <span>Nem</span>
+                                        </li>
+                                        <li class="d-flex justify-content-between align-items-center">
+                                            <span>Wi-fi</span>
+                                            <span>Igen</span>
+                                        </li>
+                                        <li class="d-flex justify-content-between align-items-center">
+                                            <span>Szoba szervíz</span>
+                                            <span>Nem</span>
+                                        </li>
+                                        <li class="d-flex justify-content-between align-items-center">
+                                            <span>Légkondicionáló</span>
+                                            <span>Igen</span>
+                                        </li>
+                                        <li class="d-flex justify-content-between align-items-center">
+                                            <span>Étterem</span>
+                                            <span>Igen</span>
+                                        </li>
+                                        <li class="d-flex justify-content-between align-items-center">
+                                            <span>Ár/éjszaka</span>
+                                            <a href="#" class="price-btn">40000Ft</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endforeach; ?>
+
+
 					</div>
 				</div>	
 			</section>
